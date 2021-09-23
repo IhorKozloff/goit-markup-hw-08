0e6275431e3d8ecd228837d82f4f86c407998280
IhorKozloff <ik.eyeshield21@gmail.com>
2021-09-23 17:58:55 +0300

    почти закончил мобайл

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,7 @@
 (() => {
     const refs = {
       openModalBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
+      closeModalBtn: document.querySelector('.modal-window__close-btn'),
       modal: document.querySelector('[data-modal]'),
     };
    
@@ -13,4 +13,16 @@
       refs.modal.classList.toggle('is-hidden');
     }
   })();
+
+  const closeBtn = document.querySelector('.contacts-menu__close-btn');
+  const openBurgerBtn = document.querySelector('.header__burger-menu-btn');
+  const menu = document.querySelector('.menu-overlay');
+  
+  openBurgerBtn.addEventListener('click', () => {
+  menu.classList.add('invisible');
+  })
+
+  closeBtn.addEventListener('click', () => {
+   menu.classList.remove('invisible');
+  })
  
---
+++ js/portfolio.js
@@ -0,0 +1,12 @@
+
+  const closeBtn = document.querySelector('.contacts-menu__close-btn');
+  const openBurgerBtn = document.querySelector('.header__burger-menu-btn');
+  const menu = document.querySelector('.contacts-menu');
+  
+  openBurgerBtn.addEventListener('click', () => {
+  menu.classList.add('active');
+  })
+
+  closeBtn.addEventListener('click', () => {
+   menu.classList.remove('active');
+  })
